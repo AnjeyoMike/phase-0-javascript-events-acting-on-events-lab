7931d7d79236bb111ef250b842c1a9e7ade71eda
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-04-12 00:07:57 +0300

    events acting on events

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+/*document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });*/
+  const theDodger = document.getElementById("dodger");
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    // Move the dodger to the right by incrementing the left position
+    dodger.style.left = `${left + 1}px`;
+}
